3077f459ed0ff9e92fd3f09a9f7119ea73a6df78
Harshal Wasnik <111900209+harshalwasnik22@users.noreply.github.com>
2023-03-24 16:46:30 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -28,26 +28,27 @@ class Solution
     { 
        vector<vector<int>> dp(n, vector<int> (W+1, 0));
        
-       for(int i=wt[0]; i<=W; i++){
-        dp[0][i] = val[0];
-    }
+       
+                for(int i=wt[0]; i<=W; i++) dp[0][i] = val[0];
     
-    for(int ind =1; ind<n; ind++){
-        for(int cap=0; cap<=W; cap++){
-            
-            int notTaken = 0 + dp[ind-1][cap];
-            
-            int taken = INT_MIN;
-            if(wt[ind] <= cap)
-                taken = val[ind] + dp[ind-1][cap - wt[ind]];
+                for(int ind =1; ind<n; ind++){
+                    for(int cap=0; cap<=W; cap++){
+                        
+                        int notTaken = 0 + dp[ind-1][cap];
+                        
+                        int taken = INT_MIN;
+                        if(wt[ind] <= cap)
+                            taken = val[ind] + dp[ind-1][cap - wt[ind]];
+                            
+                        dp[ind][cap] = max(notTaken, taken);
+                    }
+                }
                 
-            dp[ind][cap] = max(notTaken, taken);
-        }
-    }
-    
     return dp[n-1][W];
     }
 };
+
+
 
 //{ Driver Code Starts.
 
